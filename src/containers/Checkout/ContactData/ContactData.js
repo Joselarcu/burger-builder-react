234dc6ba46eb1444c61a47e041d5e0bf96457e4b
Jose <jose.lara.cuadrado@gmail.com>
2020-02-09 12:22:24 +0100

    contact data

---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import axios from '../../axios-orders';
+
+import Button from '../../../components/UI/Button/Button';
+
+import classes from './ContactData.css'
+
+class ContactData extends Component {
+    state = {
+        name: '',
+        email: '',
+        address: {
+            street: '',
+            postalCode: ''
+        },
+        loading: false
+    }
+
+    orderHandler = (event) => {
+        event.preventDefault();
+        this.setState( {loading: true});
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            customer: {
+                name: 'Joe',
+                address: {
+                    street: 'Street 1',
+                    zipcode: '3244'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+
+        axios.post('/orders.json', order)
+            .then( response => {
+                this.setState( {loading: false, purchasing: false})
+            })
+            .catch(error => {
+                this.setState( {loading: false, purchasing: false});
+            })
+    }
+
+    render () {
+        return (
+          <div className={classes.ContactData}>
+            <h4>Enter yout Contact Data</h4>
+            <form>
+              <input className={classes.Input} type="text" name="name" placeholder="Your name" />
+              <input className={classes.Input} type="email" name="email" placeholder="Your email" />
+              <input className={classes.Input} type="text" name="street" placeholder="Your Street" />
+              <input className={classes.Input} type="text" name="postalcode" placeholder="Postal Code" />
+            </form>
+              <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+          </div>
+        );
+    }
+}
+
+export default ContactData;
